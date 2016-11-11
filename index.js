7c27bdf3d4d880db7b9dd417d8215072fe75855a
Jimeno0 <carlospj@icloud.com>
2016-11-11 12:22:17 +0100

    counting score

--- index.js
+++ index.js
@@ -2,27 +2,12 @@ var Question = require('./question.js');
 var Quiz = require('./quiz.js');
 var read = require('read');
 
-var q = new Question('whats your name?','Carlos',10);
-var q2 = new Question('whats your alter name?','jimeno0',5);
-var q3 = new Question('whats your age?','27',8);
+var q = new Question('whats your name?','Carlos',1);
+var q2 = new Question('whats your alter name?','jimeno0',1);
+var q3 = new Question('whats your age?','27',1);
 
 var quizArr = [q,q2,q3];
 
 var quiz = new Quiz (quizArr);
 
-
-// var questions = quiz.qs;
-//
-// for (var i = 0; i < questions.length; i++) {
-//   console.log(questions[i].q);
-//   read(questions[i].q, answer.bind(questions[i]));
-// }
-//
-// function answer( error, response) {
-//   console.log('your response for '+ this.q + ' is: ' + response);
-// }
-
-// while (quiz.level<quiz.qs.length) {
-//
-// }
-  quiz.ask();
+quiz.ask();
--- quiz.js
+++ quiz.js
@@ -3,6 +3,7 @@ var read = require('read');
 function Quiz(questions) {
   this.qs = questions;
   this.level = 0;
+  this.score = 0;
 }
 
 Quiz.prototype.addq = function (question) {
@@ -14,21 +15,18 @@ Quiz.prototype.ask = function () {
     var question_text = {prompt: this.qs[this.level].q};
 
     read(question_text, answer.bind(this));
+  } else {
+    console.log('Game finished, total score: ' + this.score);
   }
 
 };
 
 function answer( error, response) {
-
-
-
     console.log('your response for '+ this.qs[this.level].q + ' is: ' + response);
-    console.log( this.level + ' level , length' +this.qs.length );
+    if (response === this.qs[this.level].a) {
+      this.score += this.qs[this.level].val;
+    }
     this.level++;
     this.ask();
-
-
-
 }
-
 module.exports = Quiz;
